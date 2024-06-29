6d33a5cea591e51c72adae3168b478a841738ec2
Harsh <harshjoshi2k@gmail.com>
2024-06-29 15:15:54 +0530

    login register and home page design done functionality left

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/login";
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/Register.tsx";
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/login",
     element: <LoginPage />,
   },
   {
-    path: "/",
-    element: <HomePage />,
+    path: "/register",
+    element: <RegisterPage />,
   },
 ]);
